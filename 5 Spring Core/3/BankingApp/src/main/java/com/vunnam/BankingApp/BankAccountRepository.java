36f39e1ead5a0edc236de5dcb723b19ebf83792f
Arjun Kher <49650889+ArjunKher@users.noreply.github.com>
2021-03-31 16:33:40 +0530

    Delete 5 Spring Core directory

--- 5 Spring Core/3/BankingApp/src/main/java/com/vunnam/BankingApp/BankAccountRepository.java
+++
@@ -1,6 +0,0 @@
-package com.vunnam.BankingApp;
-
-public interface BankAccountRepository {
-	public double getBalance(long accountId);
-	public double updateBalance(long accountId, double newBalance);
-}
