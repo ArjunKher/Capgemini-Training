0f9afa5578fd95dfd1e01b2a3280cb26a3ed7b00
Arjun Kher <49650889+ArjunKher@users.noreply.github.com>
2021-03-31 16:33:26 +0530

    Delete Spring Security directory

--- Spring Security/SecurityAssignment/src/main/java/com/vunnam/security/UserRepo.java
+++
@@ -1,8 +0,0 @@
-package com.vunnam.security;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface UserRepo extends JpaRepository<User,Long> {
-	User findByUsername(String username);
-}
